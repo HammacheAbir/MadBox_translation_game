13f24e23a71aaabee890741ae9be8b20e38c51f8
Abir <abir.hammache@dataimpact.io>
2024-06-21 00:47:05 +0100

    added zod validation

--- backend/src/routes/player.route.ts
+++ backend/src/routes/player.route.ts
@@ -1,8 +1,11 @@
 import express, { Router } from "express"
 import {listBestPlayers,addPlayerToLeaderboard} from "../controllers/player.controler"
+import { validateRequest } from "../middlewares/validation"
+import {z} from "zod"
 
 export const router:Router = express.Router() 
 
 router.get("/leaderboard",listBestPlayers)
 
-router.post("/",addPlayerToLeaderboard)
+router.post("/",addPlayerToLeaderboard
+)
